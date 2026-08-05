7bdf2a575ef5a2f2c94b251b4bf8eb420f3e232b
shubham <shubhamshinde7792@gmail.com>
2026-08-06 01:03:24 +0530

    blocked screenshots

--- android/app/src/main/java/com/example/nextone/MainActivity.java
+++ android/app/src/main/java/com/example/nextone/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.nextone;
 
+import android.os.Bundle;
+import android.view.WindowManager;
+
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+    }
 }
